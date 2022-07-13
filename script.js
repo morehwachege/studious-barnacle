002717ba232850522d6f537d685f02af36010603
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-07-13 11:45:24 +0300

    calculate total income

--- script.js
+++ script.js
@@ -11,10 +11,15 @@ class Production {
     }
     totalProduction() {
         console.log(`Your production in Shed A is ${this.shedA} litres per day `);
-        console.log(`Your production in Shed A is ${this.shedB} litres per day `);
-        console.log(`Your production in Shed A is ${this.shedC} litres per day `);
-        console.log(`Your production in Shed A is ${this.shedD} litres per day `);
+        console.log(`Your production in Shed B is ${this.shedB} litres per day `);
+        console.log(`Your production in Shed C is ${this.shedC} litres per day `);
+        console.log(`Your production in Shed D is ${this.shedD} litres per day `);
+    }
+    incomeOverTime(selling_price, time){
+        return `Ksh. ${selling_price * time * this.total}`
     }
 }
 let prod =  new Production(12, 3, 454, 32);
-console.log(prod.returnTotal())
+console.log(prod.returnTotal())  // total amount of milk
+// total income with time
+console.log(prod.incomeOverTime(45, 2))
