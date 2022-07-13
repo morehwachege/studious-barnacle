b4393356ff1d18617a0ef6299f328153c259d632
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-07-13 11:40:20 +0300

    remove total production fn

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-let totalProduction = function (shedA, shedB, shedC, shedD) {
-    console.log(`Your production in Shed A is ${shedA} litres per day `);
-    console.log(`Your production in Shed A is ${shedB} litres per day `);
-    console.log(`Your production in Shed A is ${shedC} litres per day `);
-    console.log(`Your production in Shed A is ${shedD} litres per day `);
-    let total = shedA + shedB + shedC + shedD;
-    console.log(`The total production is ${total} litres per day`);
-    let totalMilk = function total() {
-        return total;
-    }
-}
 class Production {
     constructor(shedA, shedB, shedC, shedD) {
         this.shedA = shedA;
