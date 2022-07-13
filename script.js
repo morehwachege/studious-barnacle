c7a57291dbc2bb4034074e41ef4c9a4896f2fd77
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-07-13 11:33:33 +0300

    ad nested fn

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ let totalProduction = function (shedA, shedB, shedC, shedD){
     console.log(`Your production in Shed A is ${shedD} litres per day `);
     let total = shedA + shedB + shedC + shedD;
     console.log(`The total production is ${total} litres per day`);
+    let totalMilk= function total(){
+        return total;
+    }
 }
-totalProduction(12,3,454,32)
+let gem = totalProduction(12,3,454,32)
 const incomeOverTime = function (rate, time){
     // time in days
+
 }
+console.log(gem)
