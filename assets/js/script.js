acb31bcdfe2f6a4c7b681528d7901d65ee866db9
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-07-19 23:06:08 +0300

    generate report

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const month = document.querySelector('#selectMilk');
 const report = document.querySelector('.report');
+const reportGen = document.querySelector('.reportGen');
 const items = document.createElement('h4');
+const itemsGen = document.createElement('h4');
 let header = document.querySelector('.main-header');
 let generate = document.querySelector('.generate')
 let monthA = {};
@@ -10,24 +12,25 @@ let shed2 = document.querySelector('#shed2');
 let shed3 = document.querySelector('#shed3');
 let shed4 = document.querySelector('#shed4');
 const months = {
-            january: 31,
-            february: 29,
-            march: 31,
-            april: 30,
-            may: 31,
-            june: 30,
-            july: 31,
-            august: 31,
-            september: 30,
-            october: 31,
-            november: 30,
-            december: 31
-        }
+    january: 31,
+    february: 29,
+    march: 31,
+    april: 30,
+    may: 31,
+    june: 30,
+    july: 31,
+    august: 31,
+    september: 30,
+    october: 31,
+    november: 30,
+    december: 31
+}
 
 
 
 report.appendChild(items);
 
+reportGen.appendChild(itemsGen);
 
 class Production {
     constructor(shedA, shedB, shedC, shedD) {
@@ -49,7 +52,7 @@ class Production {
     incomeOverTime(selling_price, time) {
         // console.log(`Your weekly income will be Ksh. ${(Math.floor((selling_price * time * this.total) / 7))}`);
         // console.log(`Your monthly income will be Ksh. ${(Math.floor((selling_price * time * this.total) / 30))}`)
-        
+
         let monthlyIncome = Object.assign({}, months); // a detached copy of months
         let monthArray = new Array();
         for (const month in months) {
@@ -57,81 +60,84 @@ class Production {
                 if (month === income) {
                     monthlyIncome[month] = (Math.floor(selling_price * months[month] * this.total)); // total income in a year
                     monthA[month] = (Math.floor(selling_price * months[month] * this.total));
-                }
-            }
+                };
+            };
             monthArray.push(month);
         };
         let listIncome = Object.keys(monthlyIncome);
         listIncome.forEach(function (item) {
             console.log(`Your income for ${item} is ${monthlyIncome[item]}`);
         })
-        monthArray = monthArray.slice(0, time) // get array of months to calculate
+        monthArray = monthArray.slice(0, time); // get array of months to calculate
         let sum = 0;
         for (let i = 0; i < monthArray.length; i++) {
             sum += monthlyIncome[monthArray[i]]
-        }
+        };
         // console.log(sum)
-        return sum
+        return sum;
     }
 }
 // let prod = new Production(shed1.value, shed2.value, shed3.value, shed4.value);
 
-submit.onsubmit = function () {
-    console.log(parseInt(shed1.value * 2));
-    console.log(Number(shed1.value));
-    console.log(typeof(shed1.value));
-    shed1 = Number(shed1.value);
-    shed2 = Number(shed2.value);
-    shed3 = Number(shed3.value);
-    shed4 = Number(shed4.value);
-
-    // shed2 = shed2.value;
-    // shed3 =shed3.value;
-    // shed4 = shed4.value
-
-    let prod = new Production(shed1, shed2, shed3, shed4);
-    console.log(prod.incomeOverTime(45,3))
-    // console.log(monthA)
-
-    if (generate) {
-        generate.addEventListener('click', () => {
-
-            setTimeout(function () {
-                header.style.display = 'none';
-                document.querySelector('form').style.display = 'none';
-                document.querySelector('.title').style.display = 'none';
-                document.querySelector('.new-title').style.display = 'block';
-
+if(submit){
+    submit.onsubmit = function () {
+        shed1 = Number(shed1.value);
+        shed2 = Number(shed2.value);
+        shed3 = Number(shed3.value);
+        shed4 = Number(shed4.value);
+    
+        let prod = new Production(shed1, shed2, shed3, shed4);
+        console.log(prod.incomeOverTime(45, 3));
+        // console.log(monthA)
+    
+        if (generate) {
+            generate.addEventListener('click', () => {
+    
+                setTimeout(function () {
+                    // header.style.display = 'none';
+                    document.querySelector('header').style.display = 'none';
+                    document.querySelector('nav').style.display = 'none';
+                    document.querySelector('form').style.display = 'none';
+                    document.querySelector('select').style.display = 'none';
+                    document.querySelector('.title').style.display = 'none';
+                    document.querySelector('.new-title').style.display = 'block';
+                    // debugger;
+    
+                    for (let key in monthA) {
+                        items.innerHTML += `The total production for ${key} is ${monthA[key]} litres. Production per day is ${monthA[key] / 30} litres<br>`;
+                    }
+                    window.print();
+                    window.location.reload();
+                });
+            })
+        }
+    
+    
+        if (month) {
+            month.addEventListener('change', () => {
+    
                 for (let key in monthA) {
-                    items.innerHTML += `The total production for ${key} is ${monthA[key]} litres. Production per day is ${monthA[key] / 30} litres<br>`;
-                }
-                window.print();
-                window.location.reload();
+                    if (month.value == key) {
+                        // console.log(`Your income for ${month.value} is ${monthA[key]}`)
+                        items.innerHTML = `Your total production for ${key} is ${monthA[key]} litres. Production per day is ${monthA[key] / 30} litres<br>`;
+                        items.style.marginTop = '30px';
+                    };
+                };
             });
-        })
-    }
-
-
-    if (month) {
-        month.addEventListener('change', () => {
-
-            for (let key in monthA) {
-                if (month.value == key) {
-                    // console.log(`Your income for ${month.value} is ${monthA[key]}`)
-                    items.innerHTML = `Your total production for ${key} is ${monthA[key]} litres. Production per day is ${monthA[key] / 30} litres<br>`;
-                    items.style.marginTop = '30px';
-                }
-            }
-        })
-    } else {
-        console.log('month not available');
-    }
-
+        } else {
+            console.log('month not available');
+        }
+        // debugger;
+        alert('Your report is ready')
+        return false;
+    
+    };
 }
+
+
 // console.log(monthA);
 // Object.keys(monthA).forEach(function (key) {
 //     items.innerHTML += `Your total production for ${key} is ${monthA[key]} litres per day <br>`;
 //     console.log(key, monthA[key]);
 // });
 // console.log(shed1.value)
-
